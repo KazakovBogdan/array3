7a28e71e0d3e7f628b0ddcabbcfaf5b5a68cf86f
Stud <Stud.n.s@nmu.one>
2021-11-16 16:26:39 +0200

    Add importlib in main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from TK_1 import array_crt
 from TK_2 import tuple_crt
 from TK_3 import average_divide_list
-from TK-4 import average_multiply_list
-from TK-5 import sqrt_my_list
+from TK_4 import average_multiply_list
+import importlib
+import sys
 
 
 def main():
@@ -12,9 +13,9 @@ def main():
     print(tuple_crt(scroll))
     print(average_divide_list(scroll))
     print(average_multiply_list(scroll))
-    print(sqrt_my_list(scroll))
+    print(importlib.import_module('TK-5').sqrt_my_list(scroll))
     return 0
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main())
